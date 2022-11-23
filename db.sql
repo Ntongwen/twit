8f8a32ccfe14fae2375b70202254f55353977fd8
marion <gabigamarion@gmail.com>
2022-11-23 05:19:37 +0000

    deploy

--- db.sql
+++ db.sql
@@ -1,3 +1,6 @@
 database connection
+<<<<<<< HEAD
 db-passward=$passord
 db-username=$user
+=======
+>>>>>>> c1df9c2 ( db connection created)
--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,6 @@
-deploy java application
+ deployjava application
 Deploy python apps
 deploy in the dev env
 deployment4
 deploymen5 master
+deploy from JBoss
